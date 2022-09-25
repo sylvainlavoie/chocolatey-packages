854b189b4475b182d40e2a7719378621db698fe9
Lavoie, Sylvain <sylvain.lavoie@changehealthcare.com>
2022-09-25 14:53:55 -0700

    foolproofed

--- Notepadplusplus.Settings/tools/chocolateyInstall.ps1
+++ Notepadplusplus.Settings/tools/chocolateyInstall.ps1
@@ -1,37 +1,53 @@
 ﻿
-$packageName = "Notepadplusplus.Settings"
+$packageName = 'Notepadplusplus.Settings'
 
-try 
-{
+try {
 	$params = $env:chocolateyPackageParameters -split ';' | ConvertFrom-StringData
 	$url = $params.url;
 	$programFiles = if (${env:ProgramFiles(x86)} -ne $null) { ${env:ProgramFiles(x86)} } else { $env:ProgramFiles }
 	
-	if ($url)
-	{
+	if ($url) {
 		Write-Host "Downloading from URL $url"
-		Get-ChocolateyWebFile $packageName "$env:appdata\Notepad++\config.xml" "$url/config.xml"
-		Get-ChocolateyWebFile $packageName "$env:appdata\Notepad++\contextMenu.xml" "$url/contextMenu.xml"
-		Get-ChocolateyWebFile $packageName "$env:appdata\Notepad++\functionList.xml" "$url/functionList.xml"
-		Get-ChocolateyWebFile $packageName "$env:appdata\Notepad++\langs.xml" "$url/langs.xml"
-		Get-ChocolateyWebFile $packageName "$env:appdata\Notepad++\shortcuts.xml" "$url/shortcuts.xml"
-		Get-ChocolateyWebFile $packageName "$env:appdata\Notepad++\stylers.xml" "$url/stylers.xml"
+		try { 
+			$settings = "$PSScriptRoot\settings"
+			Remove-Item -Path $settings -Force -ErrorAction SilentlyContinue
+			$null = New-Item -Path $settings -ItemType Directory -Force -ErrorAction SilentlyContinue
+
+			Get-ChocolateyWebFile $packageName "$settings\config.xml" "$url/config.xml"
+			Get-ChocolateyWebFile $packageName "$settings\contextMenu.xml" "$url/contextMenu.xml"
+			Get-ChocolateyWebFile $packageName "$settings\functionList.xml" "$url/functionList.xml"
+			Get-ChocolateyWebFile $packageName "$settings\langs.xml" "$url/langs.xml"
+			Get-ChocolateyWebFile $packageName "$settings\shortcuts.xml" "$url/shortcuts.xml"
+			Get-ChocolateyWebFile $packageName "$settings\stylers.xml" "$url/stylers.xml"
+
+			$params = @("$settings\", "$env:appdata\Notepad++\", '/MOVE', '/E', '/IS', '/r:10', '/w:10', '/NP',  '/NC', '/ETA', '/FP' )
+    
+			& c:\windows\System32\Robocopy.exe $params 
 		
-		# Find the theme file
-		$themeNode = Select-Xml -Path "$env:appdata\notepad++\config.xml" -XPath "//GUIConfig[@name='stylerTheme']"
-		$themePath = $themeNode.node.path
-		$themeFile = Split-Path $themePath -Leaf
-		Get-ChocolateyWebFile $packageName "$programFiles\Notepad++\themes\$themeFile" "$url/$themeFile"
-	}
-	else 
-	{
+			# Find the theme file
+			$themeNode = Select-Xml -Path "$env:appdata\notepad++\config.xml" -XPath "//GUIConfig[@name='stylerTheme']"
+			$themePath = $themeNode.node.path
+			$themeFile = Split-Path $themePath -Leaf
+			Get-ChocolateyWebFile $packageName "settings\$themeFile" "$url/$themeFile"
+			if (Test-Path -Path "${env:ProgramFiles(x86)}\Notepad++") {
+				$dest = "${env:ProgramFiles(x86)}\Notepad++"
+			}
+			if (Test-Path -Path "${env:ProgramFiles}\Notepad++") {
+				$dest = "${env:ProgramFiles}\Notepad++"
+			}
+			if ($dest) { 
+				$params = @("$settings\", "$dest\", '/MOVE', '/E', '/IS', '/r:10', '/w:10', '/NP',  '/NC', '/ETA', '/FP' )
+				& c:\windows\System32\Robocopy.exe $params 
+			}
+		} catch {
+			Write-Host "$packageName exception ignored [$_]"
+		}
+	} else {
 		throw "No URL specified. Try calling choco install Notepadplusplus.Settings -params 'url=http://example.com/NotepadSettings/'"
 	}
 	
 	Write-ChocolateySuccess "$packageName"
-} 
-catch 
-{
+} catch {
 	Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
 	throw
 }
